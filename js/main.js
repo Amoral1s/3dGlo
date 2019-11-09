437649bb3596b9e6d198a00e241e170c01a9a559
Dmitriy Filippov <Amoralis.19@gmail.com>
2019-11-09 13:24:56 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () { 
+
+  'use strict';
+
+  function countTimer(deadline) { 
+    let timeHours = document.querySelector('#timer-hours'),
+        timeMinutes = document.querySelector('#timer-minutes'),
+        timeSeconds = document.querySelector('#timer-seconds');
+
+
+        function getTimeRemaining() {
+          let dateStop = new Date(deadline).getTime(),
+              dateNow = new Date().getTime(),
+              timeRemaining = (dateStop - dateNow) / 1000,
+              seconds = ('0' +Math.floor(timeRemaining % 60)).slice(-2),
+              minutes = ('0' +Math.floor((timeRemaining / 60) % 60)).slice(-2),
+              hours = ('0' + Math.floor((timeRemaining / 60 / 60) % 24)).slice(-2);
+              return {timeRemaining,hours,minutes,seconds};
+
+        }
+        
+        function updateClock() {
+            let timer = getTimeRemaining();
+            timeHours.textContent = timer.hours;
+            timeMinutes.textContent = timer.minutes;
+            timeSeconds.textContent = timer.seconds;
+
+            if (timer.timeRemaining > 0) {
+            setTimeout(updateClock, 1000);
+            } else {
+              timeHours.textContent = '00';
+              timeMinutes.textContent = '00';
+              timeSeconds.textContent = '00';
+            }
+        }
+        updateClock();
+  }
+  countTimer('11 nov 2019');
+});
